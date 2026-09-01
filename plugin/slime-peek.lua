30a65580be188776179ef5819518beb01c73d912
Erik Fasterius <erik.fasterius@outlook.com>
2026-09-01 06:57:47 +0200

    Add check to see if plugin is already loaded

--- plugin/slime-peek.lua
+++ plugin/slime-peek.lua
@@ -1,3 +1,8 @@
+if vim.g.loaded_slime_peek then
+    return
+end
+vim.g.loaded_slime_peek = 1
+
 if vim.fn.has("nvim-0.7.0") ~= 1 then
     vim.api.nvim_err_writeln("slime-peek.nvim requires at least Neovim v0.7.0.")
 end
